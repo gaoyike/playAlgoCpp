403ede5b166f8579e76e6298e7916da0d75f6332
gaoyike <gaoyike@gmail.com>
2020-07-09 18:25:55 -0500

    update

--- playground/test_playground.cpp
+++ playground/test_playground.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <deque>
+#include <vector>
 #include <set>
+#include <unordered_map>
+#include <string>
 using namespace std;
 int main()
 {
-    int i = -1;
-    int b = 2;
-    cout << i << " " << b << ends; 
+    vector<int> v1 = {5,5555,3,3};
+    vector<int> v2 = {5,22222,3,3};
+    auto p = mismatch(v1.begin(), v1.end(), v2.begin());
+    cout <<*p.first<<" "<< *p.second<< endl;
     return 0;
 }
