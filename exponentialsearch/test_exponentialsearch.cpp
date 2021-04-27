978418b124231c69df48dc06d889d2b1e249c63e
gaoyike <gaoyike@gmail.com>
2021-04-27 14:29:52 -0500

    upgrade to win64

--- exponentialsearch/test_exponentialsearch.cpp
+++ exponentialsearch/test_exponentialsearch.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int N = sizeof(a) / sizeof(a[0]);
     cout << exponentialsearch(a,  N, 6) << endl;
--- playground/test_playground.cpp
+++ playground/test_playground.cpp
@@ -1,27 +1,16 @@
 #include <iostream>  // std::cout
 #include <algorithm> // std::lower_bound, std::upper_bound, std::sort
 #include <vector>    // std::vector
+#include <queue>    // std::priority_queue
+#include <float.h>
+#include <cstdlib>
+#include <bits/stdc++.h> 
 using namespace std;
 
-struct Node
-{
-    int key;
-    int val;
-    Node() : key(0), val(0)
-    {
-        cout << "default" << endl;
-    }
-    Node(int k, int v) : key(k),
-                         val(v)
-    {
-        cout << "parameters" << endl;
-    }
-};
 int main()
-{
-    Node n1;
-    cout << "n1 " << n1.key << n1.val << endl;
-    Node n2(555, 666);
-    cout << "n2 " << n2.key << n2.val << endl;
-    return 0;
+{ 
+    int t;
+    cin >> t;
+    cout << t;
+    return 0; 
 }
