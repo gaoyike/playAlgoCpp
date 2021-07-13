7d942a5e539100763b493c75b40ee20b50e6b5c1
gaoyike <gaoyike@gmail.com>
2021-07-13 10:01:25 -0500

    731

---
+++ playground/731C.cpp
@@ -0,0 +1,88 @@
+#include "bits/stdc++.h"
+using namespace std;   
+// fast read
+const auto fr = [](){
+    std::ios_base::sync_with_stdio(0); std::cin.tie(0);
+    std::cout << std::fixed << std::setprecision(12);
+    return 1;
+}();
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.first << ", " << p.second << ")"; };
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+	cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+	cin >> p.first;
+	return cin >> p.second;
+}
+// vars:
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = std::vector<int>;
+using vl = std::vector<ll>;
+using vvi = std::vector<vi>;
+using vvl = std::vector<vl>;
+using pii = std::pair<int,int>;
+using pil = std::pair<int,ll>;
+using pli = std::pair<ll,int>;
+using pll = std::pair<ll,ll>;
+using vpii = std::vector<pii>;
+using vvpii = std::vector<vpii>;
+// consts
+ll M = 0;
+// ksm (kuai su mi)
+ll ksm(ll a,ll p){ll res=1;while(p){if(p&1){res=res*a%M;}a=a*a%M;p>>=1;}return res;}
+ll gcd(ll a, ll b){if(b == 0) return a; return gcd(b, a % b);}
+ll lcm(ll a, ll b){return a * b / gcd(a, b);}
+
+int main() { 
+    fr; 
+    int T;
+	cin >> T;
+	while (T--)
+	{ 
+		int K,N,M;
+		cin >> K >> N >> M;
+		int A[N];
+		for(int i = 0; i < N; i++)
+			cin >> A[i];
+		int B[M];
+		for(int i = 0; i < M; i++)
+			cin >> B[i];
+		int i = 0, j = 0;
+		ll max = K;
+		int flag = 1;
+		vector<int> v;
+		while (flag)
+		{
+			flag = 0;
+			while (i < N && A[i] <= max)
+			{
+				if(A[i] == 0)
+					max++;
+				v.push_back(A[i++]);
+				flag = 1;
+			}
+			while (j < M && B[j] <= max)
+			{
+				if(B[j] == 0)
+					max++;
+				v.push_back(B[j++]);
+				flag = 1;
+			}
+		}
+		if(i == N && j == M)
+		{
+			for(int i = 0; i < v.size(); i++)
+				cout << v[i] << " ";
+			cout << endl;
+		}
+		else
+		{
+			cout << -1 << endl;
+		}
+		
+	}
+    return 0; 
+}
